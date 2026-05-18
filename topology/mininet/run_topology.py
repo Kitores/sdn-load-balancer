ca5b076957d47cd94d1c29bea0b461789b037a00
Kitores <mihail_yermolayev@mail.ru>
2026-05-18 16:12:20 +0300

    added service3 to faucet.yaml, refactor topology

--- topology/mininet/run_topology.py
+++ topology/mininet/run_topology.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python3
-from mininet.net import Containernet
 from mininet.node import RemoteController, OVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink
+
+import sys
+sys.path.append('/opt/containernet')
+from mininet.net import Containernet
+
 
 def myNetwork():
     net = Containernet(controller=RemoteController)
@@ -17,26 +20,38 @@ def myNetwork():
     s1 = net.addSwitch('s1', cls=OVSSwitch, protocols='OpenFlow13')
 
     info('*** Adding docker containers\n')
-    # Твои Go-сервисы
-    d1 = net.addDocker('d1', ip='10.0.0.1', dimage="service1:latest")
-    d2 = net.addDocker('d2', ip='10.0.0.2', dimage="service2:latest")
-    d3 = net.addDocker('d3', ip='10.0.0.3', dimage="service3:latest")
-    
+    d1 = net.addDocker('d1', ip='10.0.0.1', dimage="service1:latest", mac='00:00:00:00:00:01')
+    d2 = net.addDocker('d2', ip='10.0.0.2', dimage="service2:latest", mac='00:00:00:00:00:02')
+    d3 = net.addDocker('d3', ip='10.0.0.3', dimage="service3:latest", mac='00:00:00:00:00:03')
+
     # Добавим "клиента", который будет генерировать нагрузку
-    # Можно использовать обычный образ ubuntu или alpine
-    client = net.addDocker('client', ip='10.0.0.10', dimage="client:latest")
+    client = net.addDocker('client', ip='10.0.0.10', dimage="client:latest", mac='00:00:00:00:00:10')
 
     info('*** Creating links\n')
     # Соединяем всё со свитчем
-    net.addLink(d1, s1)
-    net.addLink(d2, s1)
-    net.addLink(d3, s1)
-    net.addLink(client, s1)
+    net.addLink(d1, s1, port1=1)
+    net.addLink(d2, s1, port1=2)
+    net.addLink(d3, s1, port1=3)
+    net.addLink(client, s1, port1=4)
 
     info('*** Starting network\n')
     net.start()
 
-    
+    intf_name1 = d1.defaultIntf().name
+    info(intf_name1)
+
+
+    info('*** Configuring hosts\n')
+    d1.cmd(f'ip addr add 10.0.0.100/32 dev {d1.defaultIntf().name}')
+    d2.cmd(f'ip addr add 10.0.0.100/32 dev {d2.defaultIntf().name}')
+    d3.cmd(f'ip addr add 10.0.0.100/32 dev {d3.defaultIntf().name}')
+    client.cmd('arp -s 10.0.0.100 00:00:00:00:00:02')
+
+    info('*** Running main processes\n')
+    d1.cmd('./main &')
+    d2.cmd('./main &')
+    d3.cmd('./main &')
+
     info('*** Running CLI\n')
     CLI(net)
 
